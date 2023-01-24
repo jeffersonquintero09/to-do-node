3f5f20598e9202541acc849260c7ceb524d6029f
Jefferson Quintero <jqb2409@gmail.com>
2023-01-23 21:10:32 -0500

    added readline

---
+++ app.js
@@ -0,0 +1,14 @@
+require('colors');
+
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
+console.clear();
+
+const main = async() => {
+    console.log('Hola Mundo');
+
+    mostrarMenu();
+    pausa();
+}
+
+main();

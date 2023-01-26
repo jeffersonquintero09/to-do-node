161dbda0880537b714441b510287f9451344eee3
Jefferson Quintero <jqb2409@gmail.com>
2023-01-25 19:52:21 -0500

    Inquirer añadido

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 require('colors');
 
-const { mostrarMenu, pausa } = require('./helpers/mensajes');
+const { inquirerMenu } = require('./helpers/inquirer');
+//const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
 console.clear();
 
 const main = async() => {
     console.log('Hola Mundo');
 
-    mostrarMenu();
-    pausa();
+    let opt = '';
+    do {
+        opt = await inquirerMenu();
+        console.log({opt});
+    } while (opt !== '0');
 }
 
 main();

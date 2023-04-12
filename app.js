fa863dab5ecdeed38447de1c0457ba720a92216f
Jefferson Quintero <jqb2409@gmail.com>
2023-04-11 20:18:24 -0500

    listado de tareas añadido

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 require('colors');
 
-const { inquirerMenu, pausa } = require('./helpers/inquirer');
+const { 
+        inquirerMenu, 
+        pausa,
+        leerInput 
+    } = require('./helpers/inquirer');
+
 const Tareas = require('./models/tareas');
 
 const main = async() => {
-    console.log('Hola Mundo');
-
+    
     let opt = '';
     const tareas = new Tareas();
 
     do {
         opt = await inquirerMenu();
-        console.log({opt});
+        //console.log({opt});
+        switch (opt) {
+            case '1':
+                const desc = await leerInput('Descripción: ');
+                tareas.crearTarea(desc);
+                break;
+        
+            case '2':
+                console.log(tareas.listadoArr);
+                break;
+        }
 
         await pausa();
     } while (opt !== '0');
